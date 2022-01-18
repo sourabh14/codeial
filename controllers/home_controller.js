d4d1212e24473c3379ba28c5b7e11b7f6f887667
Sourabh Shrivastava <simplysourabh_123@yahoo.com>
2022-01-18 13:08:37 +0530

    setup static file access for layouts and pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home = (req, res) => {
-    return res.end('<h1> Express is up for Codeial! </h1>');
+    return res.render('home', {
+        title: "Home"
+    });
 }
--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
 const app = express();
 const port = 8000;
 
+// Use express layout
+app.use(expressLayouts);
+
 // Use express router
 app.use('/', require("./routes"));
+
+// Path for static files
+app.use(express.static('./assets'));
+
+// Extract style and scripts from subpages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+// Set EJS as view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, (err) => {
     if (err) {
