3ba69c852596534402d72598716771d8876792f8
Sourabh Shrivastava <simplysourabh_123@yahoo.com>
2022-01-20 23:44:03 +0530

    Show posts in homepage

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,13 @@
+const Post = require('../models/posts')
+
 module.exports.home = (req, res) => {
-	console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
-    });
+	// console.log(req.cookies);
+    // res.cookie('user_id', 25);
+    
+    Post.find({}, function(err, posts) {
+        return res.render('home', {
+            title: 'Home',
+            posts: posts
+        })
+    })
 }
