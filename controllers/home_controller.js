5298a08c7526ef29b0cac4e2be778194de0bc183
Sourabh Shrivastava <simplysourabh_123@yahoo.com>
2022-01-21 08:05:09 +0530

    Display username and post timestamp

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,13 +1,24 @@
-const Post = require('../models/posts')
+const Post = require('../models/posts');
+const moment = require('moment');
 
 module.exports.home = (req, res) => {
 	// console.log(req.cookies);
     // res.cookie('user_id', 25);
     
-    Post.find({}, function(err, posts) {
+    // Post.find({}, function(err, posts) {
+    //     return res.render('home', {
+    //         title: 'Home',
+    //         posts: posts,
+    //         moment: moment
+    //     })
+    // })
+
+    // Populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts) {
         return res.render('home', {
             title: 'Home',
-            posts: posts
-        })
-    })
+            posts: posts,
+            moment: moment
+        });
+    });
 }
