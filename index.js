86755c9ba7954e510a2a4ff1d180dd7f600b1f10
Sourabh Shrivastava <simplysourabh_123@yahoo.com>
2022-02-13 14:08:52 +0530

    Change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 80;
+const port = 8000;
 
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
